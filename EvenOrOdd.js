5fb69d2665fc0c34199e02277314b3168228acb5
Yuriy Lipchey <xaosmmm@gmail.com>
2021-02-19 16:12:21 +0200

    Added ternary operator solution for EvenOdd function

--- EvenOrOdd.js
+++ EvenOrOdd.js
@@ -1,11 +1,17 @@
 //check if number is even or odd
 
+// function evenOrOdd(inputNumber) {
+//     if (!isFinite(inputNumber)) return 'incorrectNumber'
+//     if (!Number.isInteger(inputNumber)) return 'notInteger'
+//     if (inputNumber % 2 === 0) return 'even'
+//     return 'odd'
+// }
+
 function evenOrOdd(inputNumber) {
     if (!isFinite(inputNumber)) return 'incorrectNumber'
     if (!Number.isInteger(inputNumber)) return 'notInteger'
-    if (inputNumber % 2 === 0) return 'even'
-    return 'odd'
+    return inputNumber % 2 === 0 ? 'even' : 'odd'
 }
 
-const input = -4
+const input = 4
 console.log(evenOrOdd(input))
